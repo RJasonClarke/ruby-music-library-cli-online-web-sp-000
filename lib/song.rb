7a1666a4098d6ffd89f3f7db6a6d0537f15f1f45
Robert Clarke <r.jason.clarke@gmail.com>
2021-05-06 15:46:53 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,38 @@
+class Song
+
+  attr_accessor :name, :song
+
+  @@all = []
+
+  def initialize(name, artist=nil)
+    @name = name
+    @songs = []
+  end
+
+  def self.all
+    @@all
+  end
+
+  def self.destroy_all
+    @@all.clear
+  end
+
+  def save
+    @@all << self
+  end
+
+  def self.create(song)
+    song = self.new(song)
+    song.save
+    song
+  end
+
+  def artist
+    @artist
+  end
+
+  def artist=(artist)
+    @artist = artist
+    artist.add_song(self)
+  end
+end
