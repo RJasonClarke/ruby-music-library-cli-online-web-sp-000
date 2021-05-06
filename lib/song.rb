477ab6a78c6ac7045ea0e2ac961bdec1f83f8f37
Robert Clarke <r.jason.clarke@gmail.com>
2021-05-06 15:54:30 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -36,4 +36,24 @@ class Song
     @genre = genre
     @genre.songs << self unless @genre.songs.include?(self)
   end
+
+  def self.find_by_name(name)
+    @@all.detect {|song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    find_by_name(name) || create(name)
+  end
+
+  def self.new_from_filename(filename)
+    info = filename.split(" - ")
+    artist, name, genre = info[0], info[1], info[2].gsub( ".mp3" , "")
+    genre = Genre.find_or_create_by_name(genre)
+    artist = Artist.find_or_create_by_name(artist)
+    self.new(name,artist,genre)
+  end
+
+  def self.create_from_filename(filename)
+     self.new_from_filename(filename).save
+  end
 end
