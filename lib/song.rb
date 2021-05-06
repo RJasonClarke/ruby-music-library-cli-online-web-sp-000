f2e1e206415fd7a2e6583c8d003ac2c98a18be5c
Robert Clarke <r.jason.clarke@gmail.com>
2021-05-06 15:56:07 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -4,9 +4,10 @@ class Song
   extend Concerns::Findable
   @@all = []
 
-  def initialize(name, artist=nil)
+  def initialize(name, artist=nil, genre = nil)
     @name = name
-    @songs = []
+    self.artist=artist if artist
+    self.genre=genre if genre
   end
 
   def self.all
